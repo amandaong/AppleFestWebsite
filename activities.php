6d2bf2af30557799243538eb7dd708645881d424
Emily Ominsky <EmilyOminsky@dhcp-uris-5688.eduroam.cornell.edu>
2016-11-18 10:52:55 -0500

    top and activities

---
+++ activities.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+
+
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>Activities</title>
+<link rel="stylesheet" type="text/css" href="css/styles!!!.css">
+</head>
+
+<?php include 'top!!!/glossary.php'; ?>
+
+<body>
+<div id="container">
+
+<div id="content">
+
+    <h2>Activities!</h2>
+    <img src="images/bannerimg.jpg" alt="bannerimage">
+    
+    <div id="center">
+
+        <div id="left_content">
+                
+            <h2>There are so many fun Activities!</h2>
+            
+            <p>Including: Fun House, Merry Go Round, Berry Go ROund, Carnival Games, Street Performers, and Festival Foods
+            </p>
+
+        </div>
+        
+        <div id="right_content">
+            <img src="images/.jpg" alt="applefest image">
+            <p class="captions">  </p>
+        </div>
+
+    </div>
+
+</div> 
+    
+</div> 
+
+</body>
+
+</html>
---
+++ top.php
@@ -0,0 +1,19 @@
+<?php
+    echo '
+      <header>
+        <div id="banner">
+          <h1>Apple Harvest Festival</h1>
+        </div>
+
+        <div id="nav_bar">
+            <span class="nav_span"><a href="index.php">Home</a></span>
+            <span class="nav_span"><a href="food.php">Food</a></span>
+            <span class="nav_span"><a href="crafts.php">Crafts</a></span>
+            <span class="nav_span"><a href="activities.php">activities</a></span>
+            <span class="nav_span"><a href="transportation.php">Transportation</a></span>
+    
+        </div>
+        </header>
+        
+    ';
+?>
