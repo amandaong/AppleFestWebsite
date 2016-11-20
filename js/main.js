f17118260d0696f2a2e9a23ee07c641841288669
Amanda Ong <amandaong33@gmail.com>
2016-11-20 16:13:57 -0500

    Made Events page with days & times

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
 $(document).ready(function(){
    var scroll_start = 0;
-   var aboutOffSetTop = $('.about').offset().top;
-   var navBarOffSetHeight = $('.nav_bar').outerHeight(true); /*returns element height + height of padding + height of border + height of margin*/
+   var titleSectOffSetHeight = $('.titleSection').outerHeight(true);
+   var navBarOffSetHeight = $('.titleSection .nav_bar').outerHeight(true); /*returns element height + height of padding + height of border + height of margin*/
    console.log(navBarOffSetHeight);
    $(document).scroll(function() { 
       scroll_start = $(window).scrollTop();
-      if(scroll_start > aboutOffSetTop - navBarOffSetHeight -15) {
-          $('.nav_bar').css('background-color', 'rgba(126, 201, 237,1)');
+      if(scroll_start > titleSectOffSetHeight - navBarOffSetHeight -10) {
+          $('.titleSection .nav_bar').css('background-color', 'rgba(126, 201, 237,1)'); /*must do .titleSection .nav_bar to specify nav_bar on home page b/c changing color of nav_bar only applies to home page nav_bar*/
        } else {
-          $('.nav_bar').css('background-color', 'transparent');
+          $('.titleSection .nav_bar').css('background-color', 'transparent');
        }
    });
 });
