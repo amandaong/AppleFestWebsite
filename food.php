78d06964f92a523229467a12f4773bc66d641407
cc987@cornell.edu <chelseachan@dhcp-hol-5193.eduroam.cornell.edu>
2016-11-28 21:26:28 -0500

    added content into food page

---
+++ food.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Apple Fest Website: Food</title>
+	<link rel="stylesheet" href="styles/styles.css" type="text/css"/>
+        <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js" type="text/javascript"></script>        
+        <script src="js/main.js"></script>
+</head>
+<body>
+	 <div class="titleSection foodTitleSection"> 
+        <div class="nav_bar">
+            <ul> <li> <a href="index.html">HOME</a> </li>
+            <li> <a href="events.html">SCHEDULE OF EVENTS</a> </li>
+            <li> <a href="food.html">FOOD</a> </li>
+            <li> <a href="crafts.html">CRAFT SHOW</a> </li>
+            <li> <a href="transportation.html">TRANSPORTATION</a> </li> 
+            </ul>
+        </div>
+            
+     	<div class = "appleFestTitle"> 
+            <div class = "foodTitle"> FOOD </div>
+        </div>
+    </div>
+
+    <div class="foodList">
+
+    	<p>A wide selection of delicious food from over 100 vendors will satisfy all cravings. Dishes range from warm curries, to pulled pork sandwiches and caramel apples. Here are some top picks to try:</p>
+
+    	<div class="foodItem">
+    		<img class="foodImage" src="images/applevariety.jpg" alt="Apples"> <!-- https://octoberat216.wordpress.com/2009/09/27/ithaca-apple-festival/ -->
+    	    <div class="title">Apples</div>
+    		<p>Of course, the festival is a great chance to try the wide selection of apples from Ithaca's farmers.</p>
+    	</div>
+    	
+    	<div class="foodItem">
+	    	<img class="foodImage" src="images/donuts.jpg" alt="Apple Cider Donuts"> <!-- http://southingtonobserver.com/2013/10/07/the-big-apple/ -->
+	    	<div class="title">Hot Apple Cider Donuts</div>
+	    	<p>Be sure to try Littletree Orchards famous apple cider donuts. The wait in line can range from 30 minutes to an hour depending on the time of day.</p>
+    	</div>
+
+    	<div class="foodItem">
+	    	<img class="foodImage" src="images/fudge.jpg" alt="Fudge"> <!-- https://www.flickr.com/photos/106103661@N02/22027343645/in/album-72157657274645113/ -->
+	    	<div class="title">Homemade Fudge</div>
+	    	<p>Another indulgent treat that's perfect for the fall. </p>
+    	</div>
+
+    	<div class="foodItem">
+	    	<img class="foodImage" src="images/farmers.jpg" alt="Farmers Market"> <!-- https://www.flickr.com/photos/106103661@N02/10397075273/in/album-72157648008079417/ -->
+	    	<div class="title">Fresh from the Farmers' Market</div>
+	    	<p>Farmers will be selling much more than apples. Local produce will range from freshly picked grapes to all-natural honey varieties.</p>
+    	</div>
+
+    	<div class="foodItem">
+	    	<img class="foodImage" src="images/ciderbottle.jpg" alt="Apple Cider"> <!-- http://www.ciderweekflx.com/event/ithacas-apple-harvest-festival/ -->
+	    	<div class="title">Apple Cider</div>
+	    	<p>A warm and comforting drink, perfect on a chilly day. It's even sold by the gallon.</p>
+    	</div>
+
+    	<img class="sign" src="images/foodsign.jpg">
+    	
+    </div>
+     
+
+</body>
+</html>
